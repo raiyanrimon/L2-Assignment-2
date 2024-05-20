400f16614abb826e03f71491a1cf666d26760d54
GOLAM RAIYAN <raiyan1615@student.nstu.edu.bd>
2024-05-21 00:48:20 +0600

    first commit

---
+++ src/app/modules/product/product.interface.ts
@@ -0,0 +1,19 @@
+interface ProductData {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags: string[];
+  variants: Variant[];
+  inventory: Inventory;
+}
+
+interface Variant {
+  type: string;
+  value: string;
+}
+
+interface Inventory {
+  quantity: number;
+  inStock: boolean;
+}
