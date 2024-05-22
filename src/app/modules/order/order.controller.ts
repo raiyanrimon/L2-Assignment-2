c5e1c5a1869fb8f5ba61d24ace6480847dbd5c68
GOLAM RAIYAN <raiyan1615@student.nstu.edu.bd>
2024-05-23 01:50:55 +0600

    order create api and validation added

--- src/app/modules/order/order.controller.ts
+++ src/app/modules/order/order.controller.ts
@@ -1,14 +1,31 @@
 import { Request, Response } from "express";
 import { OrderServices } from "./order.service";
 import { orderValidationDataSchema } from "./order.validation";
+import { ProductModel } from "../product/product.model";
 
 const createOrdersIntoDB = async (req: Request, res: Response) => {
   try {
     const { order: OrderData } = req.body;
+    const { productId, quantity } = OrderData;
+    const product = await ProductModel.findById(productId);
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    if (product.inventory.quantity < quantity) {
+      return res.status(400).json({
+        success: false,
+        message: "Insufficient quantity available in inventory",
+      });
+    }
+    product.inventory.quantity -= quantity;
+    product.inventory.inStock = product.inventory.quantity > 0;
+    await product.save();
     const zodParsedOrder = orderValidationDataSchema.parse(OrderData);
     const result = await OrderServices.createOrdersIntoDB(zodParsedOrder);
-    if (result) {
-    }
     res.status(200).json({
       success: true,
       message: "Order created successfully!",
@@ -17,7 +34,7 @@ const createOrdersIntoDB = async (req: Request, res: Response) => {
   } catch (err) {
     res.status(500).json({
       success: false,
-      message: "An Error occurred",
+      message: "ProductId not found",
     });
   }
 };
